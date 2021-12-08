9499b54d82b0fdbec7e841bb196b157c7f391207
Chen Liqiang (aka Tan Lee Chiang) <72908309+tanleech2020@users.noreply.github.com>
2021-12-08 17:53:05 +0800

    Add files via upload
    initial version

---
+++ mgmt/src/main/java/com/insurance/mgmt/MgmtApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgmtApplication.class, args);
+	}
+
+}
